173af8f7612ed0d410a108a72e33f92084ba5ffc
Alexbpx <alexbulnespovea@gmail.com>
2025-11-12 19:23:19 +0100

    Aportación de Alejandro y Raúl

---
+++ proyecto1.7/Consulta.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE fila_2;
+
+USE fila_2;
+
+CREATE TABLE IPV (id INT AUTO_INCREMENT PRIMARY KEY,
+                  COD VARCHAR(20) NOT NULL,
+                  Nombre VARCHAR(200) NOT NULL);
+
+
+
+CREATE TABLE data_ipv (id INT AUTO_INCREMENT PRIMARY KEY,
+                        Fecha VARCHAR(200) NOT NULL,
+                              FK_Periodo INT,
+                              Anyo VARCHAR(5),
+                              Valor FLOAT,
+                              Id_ipv INT,
+                              FOREIGN KEY (Id_ipv) REFERENCES ipv (id));
+
