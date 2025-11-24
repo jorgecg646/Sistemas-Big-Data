3fc5eeb2ee774ab26970ce81656e03e11167deb6
Alexbpx <alexbulnespovea@gmail.com>
2025-11-24 19:24:16 +0100

    Subimos datos IPC

--- proyecto1.7/Consulta.sql
+++ proyecto1.7/Consulta.sql
@@ -16,3 +16,18 @@ CREATE TABLE data_ipv (id INT AUTO_INCREMENT PRIMARY KEY,
                               Id_ipv INT,
                               FOREIGN KEY (Id_ipv) REFERENCES ipv (id));
 
+
+
+CREATE TABLE IPC (id INT AUTO_INCREMENT PRIMARY KEY,
+                  COD VARCHAR(20) NOT NULL,
+                  Nombre VARCHAR(200) NOT NULL);
+
+
+
+CREATE TABLE data_ipc (id INT AUTO_INCREMENT PRIMARY KEY,
+                        Fecha VARCHAR(200) NOT NULL,
+                              FK_Periodo INT,
+                              Anyo VARCHAR(5),
+                              Valor FLOAT,
+                              Id_ipc INT,
+                              FOREIGN KEY (Id_ipc) REFERENCES ipc (id));
